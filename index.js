ff695b5e8a57a9305a7d19fa0a0c844b7ce5abd6
Somiya <isomiyasj2@gmail.com>
2025-10-08 17:01:22 +0530

    pod name added

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const app = express();
 
+app.use((req, res, next) => {
+  console.log(`Request received by pod: ${process.env.POD_NAME}, path: ${req.path}`);
+  next();
+});
+
+
 app.get('/hello', (req, res) => {
-  res.send('hello');
+  res.send(`hello from pod: ${process.env.POD_NAME}`);
 });
 
 const PORT = process.env.PORT || 3000;
